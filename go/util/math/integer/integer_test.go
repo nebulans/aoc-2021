83e8dd30d697efef499a52084a7503bc02731f2d
Tim Davies <tim@timdavies.xyz>
2021-12-06 22:08:09 +0000

    Tests for sum methods

--- go/util/math/integer/integer_test.go
+++ go/util/math/integer/integer_test.go
@@ -2,6 +2,7 @@ package integer
 
 import (
 	"fmt"
+	"math/big"
 	"testing"
 )
 
@@ -64,6 +65,48 @@ func TestUnitStep(t *testing.T) {
 		t.Run(testName, func(t *testing.T) {
 			ans := UnitStep(tt.a)
 			if ans != tt.want {
+				t.Errorf("got %d, want %d", ans, tt.want)
+			}
+		})
+	}
+}
+
+func TestSumFunctions(t *testing.T) {
+	var tests = []struct {
+		a    []int
+		want int
+	}{
+		{[]int{}, 0},
+		{[]int{0}, 0},
+		{[]int{2, -1, -1}, 0},
+		{[]int{10}, 10},
+		{[]int{2, 3, 10}, 15},
+	}
+	for _, tt := range tests {
+		testName := fmt.Sprintf("sum(%v)==%d", tt.a, tt.want)
+		t.Run(testName, func(t *testing.T) {
+			ans := Sum(tt.a)
+			if ans != tt.want {
+				t.Errorf("got %d, want %d", ans, tt.want)
+			}
+		})
+		uints := make([]uint64, len(tt.a))
+		for i, v := range tt.a {
+			uints[i] = uint64(v)
+		}
+		t.Run(fmt.Sprintf("SumUint64(%v)==%d", uints, tt.want), func(t *testing.T) {
+			ans := SumUint64(uints)
+			if int(ans) != tt.want {
+				t.Errorf("got %d, want %d", ans, tt.want)
+			}
+		})
+		bigints := make([]*big.Int, len(tt.a))
+		for i, v := range tt.a {
+			bigints[i] = big.NewInt(int64(v))
+		}
+		t.Run(fmt.Sprintf("SumBigInt(%v)==%d", bigints, tt.want), func(t *testing.T) {
+			ans := SumBigInt(bigints)
+			if int(ans.Int64()) != tt.want {
 				t.Errorf("got %d, want %d", ans, tt.want)
 			}
 		})
